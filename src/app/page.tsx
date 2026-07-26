84417d2efe5eb5ac91be3fa15a42bb9a02a9e838
BasthDev <basithachmed2@gmail.com>
2026-07-26 14:59:00 +0700

    adding logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -839,7 +839,7 @@ export default function App() {
                   style={{
                     width: "100%",
                     height: "100%",
-                    objectFit: "contain",
+                    objectFit: "cover",
                   }}
                 />
               </div>
